8164a13f214640c4c15778a622f0344e3de9f73f
Yudetamago-AM <akio@rittai.org>
2022-05-05 16:41:55 +0900

    distinguish the meaning of "temperature"

---
+++ Experiments/MotorTest/BNO055_temp.ino
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include "./PinAssign.h"
+
+void setup() {
+    /*serial initialize*/
+    Serial.begin(9600);
+    Serial.println(F("serial begin"));
+
+    /*BNO055 initialize*/
+    while (!bno.begin()) {
+        Serial.println("couldn't detect BNO055");
+        delay(50);
+    }
+    bno.setExtCrystalUse(true);
+}
+
+void loop() {
+    /*print temperature every seconds*/
+    Serial.println(bno.getTemp());
+    delay(1000);
+}
