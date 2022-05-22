a10ff1751d5b8c8942593dfbb5619d0feb417cd7
Yudetamago-AM <akio@rittai.org>
2022-05-22 22:07:11 +0900

    add test cases

--- Experiments/MotorTest/MotorTest.ino
+++ Experiments/MotorTest/MotorTest.ino
@@ -2,20 +2,39 @@
 #include <PinAssign.h>
 #include <Motor.h>
 
-
 void setup() {
+    /*serial (for debug) initialize*/
+    Serial.begin(9600);
+    Serial.println(F("Serial begin"));
+
     motor_init();
+    Serial.println("motor init done");   
 }
 
 void loop() {
+    
+    //直進（曲がる）
     motor_foward(100, 100);// pwm 100
-    delay(2000);
+    delay(4000);
+    motor_stop();
+    delay(500);
+    
+    motor_foward(178, 255);
+    delay(3000);
+    motor_stop();
+    delay(500);
+    
+    motor_foward(255, 178);
+    delay(3000);
+    motor_stop();
+    delay(500);
 
+    motor_foward(100,0);
+    delay(1000);
+    motor_stop();
+
+    motor_foward(0, 100);
+    delay(1000);
     motor_stop();
     delay(2000);
-
-    motor_rotate(20);// 20 degree
-    delay(2000);
-
-    stop();
 }
