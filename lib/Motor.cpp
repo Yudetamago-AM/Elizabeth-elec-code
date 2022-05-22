26157e39a4f7b08e74fae4f2643e2ecf237f613e
Yudetamago-AM <akio@rittai.org>
2022-05-22 22:06:00 +0900

    motor rotates well!

--- lib/Motor.cpp
+++ lib/Motor.cpp
@@ -24,15 +24,15 @@ void motor_foward(byte pwmR, byte pwmL) {
     analogWrite(PIN_MO_L_A, pwmL);
 }
 
-/*
-void motor_back(int pwm) {
+
+void motor_back(byte pwmR, byte pwmL) {
     digitalWrite(PIN_MO_R_A, LOW);
     digitalWrite(PIN_MO_L_A, LOW);
 
-    analogWrite(PIN_MO_R_B, pwm);
-    analogWrite(PIN_MO_L_B, pwm);
+    analogWrite(PIN_MO_R_B, pwmR);
+    analogWrite(PIN_MO_L_B, pwmL);
 }
-*/
+
 
 void motor_stop() {
     // すべてLOW
@@ -61,87 +61,4 @@ void motor_foward_d(int distance) {
 }
 */
 
-/*Rotate to the ABSOLUTE angle (in euler)*/
-void motor_rotate(float angle, float angleNow) {
-    /*旋回速度，実験で決定する*/
-    byte rotatePWM = 100;
-    /*
-    PIN_MO_L/R_Bはそれぞれ5,6ピンを使っているので，デューティー比が若干高くなるそう（下リファレンス）．
-    http://www.musashinodenpa.com/arduino/ref/index.php?f=0&pos=2153
-
-    具体的には，5/6ピンから977Hz
-    9/10ピンから490Hz
-    3/11ピンから490Hzという3つがあり，490*2 = 980のためと思われる．
-    （一応）その調整をする．
-    */
-    /*
-    int offsetPWM = 1;//（およそ）1秒(50ms*20)に1回**offsetPWMの値**を引いたPWMでうごかす
-    int count = 0;
-    // メモリないのでやめにした
-    */
-    //いい感じに9軸センサー呼び出す感じ
-    //cf1. https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/arduino-code
-    //cf2. https://forums.adafruit.com/viewtopic.php?f=19&t=91723&p=462337#p462337
-    /*
-    ↑では，
-    sensors_event_t rotate;
-    bno.getEvent(&rotate);
-    みたいに書いてあるが，
-    https://learn.adafruit.com/using-the-adafruit-unified-sensor-driver/how-does-it-work#void-getevent-sensors-event-t-star
-    にあるように，Quternionはgeteventでは取得出来ない（=Raw Sensor Dataとして取得する必要がある）
-    （なんのための"Unified" Sensor Systemだ？？，下手に使えんでないか）
-    */
-    //cf3. http://l52secondary.blog.fc2.com/blog-entry-50.html
-    
-    /*
-    そのままEulerで取得するとpitch(y)かroll(x)軸が±45度を超えると，まともに使えるデータでなくなるから
-    クォータニオンで値を取得してオイラー角に変換する
-    ちなみに，（正）「クォータニオン」（誤）「クォータ二オン」，（誤）はニが2になってる，おのれATOK！
-    */
-
-
-    /*
-    imu::Quaternion q_orientation_now = bno.getQuat();
-    q_orientation_now.normalize();//重力分を取り除く（vector.h）
-    //呼び出す側で処理
-    */
-
-
-    /*入れ替え
-    float temp = q_orientation_now.x();
-    q_orientation_now.x() = -q_orientation_now.y();
-    q_orientation_now.y() = temp;
-    q_orientation_now.z() = -q_orientation_now.z();
-    //おそらく不要
-    */
-
-
-    /*
-    imu::Vector<3> e_orientation_now = q_orientation_now.toEuler();
-    float angleNow = radPI * e_orientation_now.z();
-    //呼び出す側で処理
-    */
-
-
-    float destAngle = angle - angleNow;
-    if (destAngle > 0) {
-        while (angleNow <= destAngle) {
-            /*右は後転，左は正転*/
-            digitalWrite(PIN_MO_L_B, LOW);
-            digitalWrite(PIN_MO_R_A, LOW);
-
-            analogWrite(PIN_MO_L_A, rotatePWM);
-            analogWrite(PIN_MO_R_B, rotatePWM);
-        }
-    } else if (destAngle < 0) {
-        while (angleNow <= destAngle) {
-            /*右は正転，左は後転*/
-            digitalWrite(PIN_MO_L_A, LOW);
-            digitalWrite(PIN_MO_R_B, LOW);
-
-            analogWrite(PIN_MO_L_B, rotatePWM);
-            analogWrite(PIN_MO_R_A, rotatePWM);
-        }
-    }
-    motor_stop();
-}
+//motor_rotateはmainに．
--- lib/Motor.h
+++ lib/Motor.h
@@ -3,16 +3,14 @@
 #define MOTOR_H
 
 #include <Arduino.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
 #include <PinAssign.h>
 
 void motor_init();
 void motor_foward(byte pwmR, byte pwmL);
 //void motor_foward_d(int distance); // distance(cm) メモリ食うので廃止
-//void motor_back(int pwm); //使わない
-void motor_rotate(float angle, float angleNow); //正面を0として±180度
+void motor_back(byte pwmR, byte pwmL); //使わない→つかう
+//void motor_rotate(float angle); //正面を0として±pi rad
+//このライブラリはただモーターを回すだけ，ということにするから，rotateはMain.inoに移行
 void motor_stop();
 
 #endif
